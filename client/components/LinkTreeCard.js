08748af70e6e78daca28960d35c47cd0509897a3
Ashutosh <kumarashutosh1956@gmail.com>
2024-05-29 18:32:55 +0530

    url format of links modified

--- client/components/LinkTreeCard.js
+++ client/components/LinkTreeCard.js
@@ -8,7 +8,8 @@ const LinkTreeCard = ({ title, url, image }) => {
         <Link
           className="flex flex-row items-center p-2 rounded-xl text-white bg-indigo-400 hover:bg-indigo-300 mb-3 mx-2 hover:translate-x-1 hover:translate-y-1 transition-all duration-500"
           target="_blank"
-          href={`https://${url}`}
+          // href={`https://${url}`}
+          href={`${url}`}
         >
           <img
             className="bg-white rounded-md p-1 w-11 mr-5"
