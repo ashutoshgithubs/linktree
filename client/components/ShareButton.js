3dd16569cddfc36ceb53131d0f5b40c05166e1ae
Ashutosh <kumarashutosh1956@gmail.com>
2024-06-01 18:21:57 +0530

    Share Modal enhamced

--- client/components/ShareButton.js
+++ client/components/ShareButton.js
@@ -1,21 +1,26 @@
-import React from 'react'
-import { useRouter } from 'next/router'
-import { toast } from 'react-toastify';
+import React from "react";
+import { useRouter } from "next/router";
+import { toast } from "react-toastify";
 
 const ShareButton = () => {
-    const router = useRouter();
-    const copyLink = ()=>{
-        // navigator.clipboard.writeText(`https://linktree-lovat-mu.vercel.app/${router.query.handle}`);
-        navigator.clipboard.writeText(`http://localhost:3000/${router.query.handle}`);
-        toast("Copied to clipboard");
-    }
+  const router = useRouter();
+  const copyLink = () => {
+    navigator.clipboard.writeText(
+      `https://linktree-lovat-mu.vercel.app/${router.query.handle}`
+    );
+    // navigator.clipboard.writeText(`http://localhost:3000/${router.query.handle}`);
+    toast("Copied to clipboard");
+  };
   return (
     <div>
-      <div className='absolute cursor-pointer top-28 left-10 bg-indigo-200 p-2 rounded-md z-10 shadow-md border-2 border-indigo-400' onClick={copyLink}>
-            <img className='w-4 ' src='/svg/share.svg' alt='share'/>
+      <div
+        className="absolute cursor-pointer top-28 left-10 bg-indigo-200 p-2 rounded-md z-10 shadow-md border-2 border-indigo-400"
+        onClick={copyLink}
+      >
+        <img className="w-4 " src="/svg/share.svg" alt="share" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ShareButton
+export default ShareButton;
